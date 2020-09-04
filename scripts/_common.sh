0efae29f81efd575e75496b6765558e85bdf85d7
ericgaspar <junk.eg@free.fr>
2020-09-04 19:02:51 +0200

    Add PHP extensions

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,6 +3,7 @@
 #=================================================
 # PERSONAL HELPERS
 #=================================================
+ pkg_dependencies="php${YNH_DEFAULT_PHP_VERSION}cURL php${YNH_DEFAULT_PHP_VERSION}iconv php${YNH_DEFAULT_PHP_VERSION}json php${YNH_DEFAULT_PHP_VERSION}libxml php${YNH_DEFAULT_PHP_VERSION}dom php${YNH_DEFAULT_PHP_VERSION}openssl php${YNH_DEFAULT_PHP_VERSION}DateTime php${YNH_DEFAULT_PHP_VERSION}PCRE php${YNH_DEFAULT_PHP_VERSION}SPL"
 
 #=================================================
 # EXPERIMENTAL HELPERS
