baa66d816e66b5e660bab0509c4447b2df0656c3
ericgaspar <junk.eg@free.fr>
2020-09-04 20:07:58 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # PERSONAL HELPERS
 #=================================================
 
- pkg_dependencies="php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-curl  php${YNH_PHP_VERSION}-cgi php${YNH_PHP_VERSION}-xmlrpc php${YNH_PHP_VERSION}gd php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-xmlrpc php${YNH_PHP_VERSION}-soap php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-readline php${YNH_PHP_VERSION}-imap php${YNH_PHP_VERSION}-tidy php${YNH_PHP_VERSION}-recode php${YNH_PHP_VERSION}-intl"
+pkg_dependencies="php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-libxml php${YNH_PHP_VERSION}-dom"
 
 YNH_PHP_VERSION="7.3"
 
