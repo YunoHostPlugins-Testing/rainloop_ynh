0e8ec726b9f0b8dd4f667d536f7faa2fa8d278a2
ericgaspar <junk.eg@free.fr>
2020-09-04 20:02:06 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,7 +3,10 @@
 #=================================================
 # PERSONAL HELPERS
 #=================================================
- pkg_dependencies="php${YNH_DEFAULT_PHP_VERSION}cURL php${YNH_DEFAULT_PHP_VERSION}iconv php${YNH_DEFAULT_PHP_VERSION}json php${YNH_DEFAULT_PHP_VERSION}libxml php${YNH_DEFAULT_PHP_VERSION}dom php${YNH_DEFAULT_PHP_VERSION}openssl php${YNH_DEFAULT_PHP_VERSION}DateTime php${YNH_DEFAULT_PHP_VERSION}PCRE php${YNH_DEFAULT_PHP_VERSION}SPL"
+
+ pkg_dependencies="php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-curl  php${YNH_PHP_VERSION}-cgi php${YNH_PHP_VERSION}-xmlrpc php${YNH_PHP_VERSION}gd php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-xmlrpc php${YNH_PHP_VERSION}-soap php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-readline php${YNH_PHP_VERSION}-imap php${YNH_PHP_VERSION}-tidy php${YNH_PHP_VERSION}-recode php${YNH_PHP_VERSION}-intl"
+
+YNH_PHP_VERSION="7.3"
 
 #=================================================
 # EXPERIMENTAL HELPERS
@@ -12,3 +15,5 @@
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
+
+
