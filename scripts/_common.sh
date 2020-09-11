d0785498bcdbf33e6be0ec47aa1a079a5f43d1d6
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2020-09-11 18:19:25 +0200

    Update scripts/_common.sh
    Co-authored-by: Kayou <pierre@kayou.io>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,9 @@
 # PERSONAL HELPERS
 #=================================================
 
-pkg_dependencies="php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-dom"
-
 YNH_PHP_VERSION="7.3"
+
+pkg_dependencies="php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-dom"
 
 #=================================================
 # EXPERIMENTAL HELPERS
